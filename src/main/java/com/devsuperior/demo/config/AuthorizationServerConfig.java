43aba8d5dddf65ad9b6f73a94f7769518e46e985
Lucas Silva <lucas_segundinho@hotmail.com>
2023-08-23 01:08:37 +0100

    OAuth and Jwt configuration. City repository, controller and service.

---
+++ src/main/java/com/devsuperior/demo/config/AuthorizationServerConfig.java
@@ -0,0 +1,254 @@
+package com.devsuperior.demo.config;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2AccessTokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.devsuperior.demo.config.customgrant.CustomPasswordAuthenticationConverter;
+import com.devsuperior.demo.config.customgrant.CustomPasswordAuthenticationProvider;
+import com.devsuperior.demo.config.customgrant.CustomUserAuthorities;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+	@Value("${security.client-id}")
+	private String clientId;
+
+	@Value("${security.client-secret}")
+	private String clientSecret;
+
+	@Value("${security.jwt.duration}")
+	private Integer jwtDurationSeconds;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain asSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		// Config OAuth server to work with spring security
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+		// @formatter:off
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+			.tokenEndpoint(tokenEndpoint -> tokenEndpoint
+				// Config token customizations 
+				.accessTokenRequestConverter(new CustomPasswordAuthenticationConverter())
+				// Config authentication, how to (authenticate, generate token and read credentials)
+				.authenticationProvider(new CustomPasswordAuthenticationProvider(
+						authorizationService(), tokenGenerator(), userDetailsService, passwordEncoder))
+				);
+
+		// Config spring security to use jwt
+		http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+		// @formatter:on
+
+		// Pass the configurations to spring security
+		return http.build();
+	}
+
+	/**
+	 * Necessary in order to the configurations work.
+	 * @return
+	 */
+	@Bean
+	public OAuth2AuthorizationService authorizationService() {
+		return new InMemoryOAuth2AuthorizationService();
+	}
+
+	/**
+	 * Necessary in order to the configurations work.
+	 * @return
+	 */
+	@Bean
+	public OAuth2AuthorizationConsentService oAuth2AuthorizationConsentService() {
+		return new InMemoryOAuth2AuthorizationConsentService();
+	}
+
+	/**
+	 * Registers and configures the client configuration
+	 * allowed to have access according to our data.
+	 * @return
+	 */
+	@Bean
+	public RegisteredClientRepository registeredClientRepository() {
+		// @formatter:off
+		RegisteredClient registeredClient = RegisteredClient
+			.withId(UUID.randomUUID().toString())
+			.clientId(clientId)
+			.clientSecret(passwordEncoder.encode(clientSecret))
+			.scope("read")
+			.scope("write")
+			.authorizationGrantType(new AuthorizationGrantType("password"))
+			.tokenSettings(tokenSettings())
+			.clientSettings(clientSettings())
+			.build();
+		// @formatter:on
+
+		return new InMemoryRegisteredClientRepository(registeredClient);
+	}
+
+	/**
+	 * Configures the token (duration).
+	 * @return
+	 */
+	@Bean
+	public TokenSettings tokenSettings() {
+		// @formatter:off
+		return TokenSettings.builder()
+			.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+			.accessTokenTimeToLive(Duration.ofSeconds(jwtDurationSeconds))
+			.build();
+		// @formatter:on
+	}
+
+	/**
+	 * Instantiates the client settings application. 
+	 * Necessary in order to the configurations work.
+	 * @return
+	 */
+	@Bean
+	public ClientSettings clientSettings() {
+		return ClientSettings.builder().build();
+	}
+
+	/**
+	 * This bean is necessary in order to the configurations work.
+	 * @return
+	 */
+	@Bean
+	public AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
+
+	/**
+	 * Token configurations and generation.
+	 * @return
+	 */
+	@Bean
+	public OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator() {
+		NimbusJwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
+		JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+		jwtGenerator.setJwtCustomizer(tokenCustomizer());
+		OAuth2AccessTokenGenerator accessTokenGenerator = new OAuth2AccessTokenGenerator();
+		return new DelegatingOAuth2TokenGenerator(jwtGenerator, accessTokenGenerator);
+	}
+
+	/**
+	 * Token customizations configuration (including claims).
+	 * @return
+	 */
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			OAuth2ClientAuthenticationToken principal = context.getPrincipal();
+			CustomUserAuthorities user = (CustomUserAuthorities) principal.getDetails();
+			List<String> authorities = user.getAuthorities().stream().map(x -> x.getAuthority()).toList();
+			if (context.getTokenType().getValue().equals("access_token")) {
+				// @formatter:off
+				context.getClaims()
+					// User profiles
+					.claim("authorities", authorities) 
+					// User name
+					.claim("username", user.getUsername());
+				// @formatter:on
+			}
+		};
+	}
+
+	/**
+	 * JwtDecoder bean configuration.
+	 * @param jwkSource
+	 * @return
+	 */
+	@Bean
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+
+	/**
+	 * RSA algorithm configuration. 
+	 * Generates the key to be used in the token. 
+	 * @return
+	 */
+	@Bean
+	public JWKSource<SecurityContext> jwkSource() {
+		RSAKey rsaKey = generateRsa();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+	}
+
+	/**
+	 * Helper to generate RSAKey to be used on the RSAKey bean.
+	 * @return
+	 */
+	private static RSAKey generateRsa() {
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+	}
+
+	/**
+	 * Helper to generate KeyPair to be used on the RSAKey.
+	 * @return
+	 */
+	private static KeyPair generateRsaKey() {
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		} catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+}
---
+++ src/main/java/com/devsuperior/demo/config/ResourceServerConfig.java
@@ -0,0 +1,116 @@
+package com.devsuperior.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class ResourceServerConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+
+	/**
+	 * Configures h2 on test profile. 
+	 * Enables the h2 console.
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	@Profile("test")
+	@Order(1)
+	public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+		return http.build();
+	}
+
+	/**
+	 * Configures the security of the requests.
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	@Order(3)
+	public SecurityFilterChain rsSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf(csrf -> csrf.disable());
+		// Enable all requests. If any should be authorized, configure on the routes.
+		http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+		// Configures security the type of security
+		http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+		// Enable cors configuration with our configurations.
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+		return http.build();
+	}
+
+	/**
+	 * Configuration to customize the token in order to work on the server.
+	 * @return
+	 */
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+		grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+		return jwtAuthenticationConverter;
+	}
+
+	/**
+	 * Cors configuration bean.
+	 * @return
+	 */
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+
+		String[] origins = corsOrigins.split(",");
+
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOriginPatterns(Arrays.asList(origins));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	/**
+	 * Cors filter configuration bean.
+	 * @return
+	 */
+	@Bean
+	FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+}
