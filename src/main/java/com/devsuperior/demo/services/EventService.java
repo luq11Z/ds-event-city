5e51ce99a92d58bd434d48a710540915f23e1d36
Lucas Silva <lucas_segundinho@hotmail.com>
2023-08-24 20:14:42 +0100

    Event repository, service, controller and valdiartions

---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+		return list.map(x -> new EventDTO(x));
+	}
+	
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event event = new Event();
+		event.setName(dto.getName());
+		event.setDate(dto.getDate());
+		event.setUrl(dto.getUrl());
+		event.setCity(new City(dto.getCityId(), null));
+		
+		event = repository.save(event);
+		
+		return new EventDTO(event);
+	}
+	
+}
